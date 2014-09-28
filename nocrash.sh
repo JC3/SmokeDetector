f5c2a8600ac9eea8d380337bc554f6d12e6ab7f5
Undo1 <parker@erwaysoftware.com>
2014-09-28 22:12:13 +0000

    change nocrash.sh to environ vars

--- nocrash.sh
+++ nocrash.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-read -p "Username: " u
-export ChatExchangeU=$u
+#read -p "Username: " u
+#export ChatExchangeU=$u
 export CEU="h"
 stty -echo
-read -p "Password: " p
-export ChatExchangeP=$p
+#read -p "Password: " p
+#export ChatExchangeP=$p
 stty echo
 while :
 do
