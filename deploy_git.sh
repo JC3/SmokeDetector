3f8a75e5876bca693421d78f8b42a4160cca747f
Undo1 <parker@erwaysoftware.com>
2014-09-28 03:10:38 +0000

    fix variables

--- deploy_git.sh
+++ deploy_git.sh
@@ -1,6 +1,6 @@
 git pull origin master
-autostartscript = "nocrash.sh"
-startPID = pgrep $autostartscript
+autostartscript="nocrash.sh"
+startPID=pgrep $autostartscript
 pkill -P $startPID
 pkill $autostartscript
 python $autostartscript
